78b1ee1f8cbd41d5411be3622b07fd02605a13a5
Raúl Jiménez <jimenezjrs@gmail.com>
2014-08-05 17:17:23 -0500

    added home and business delegation fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804211233) do
+ActiveRecord::Schema.define(version: 20140805213149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20140804211233) do
     t.string   "special_filters"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "home_delegation"
+    t.string   "business_delegation"
   end
 
 end
