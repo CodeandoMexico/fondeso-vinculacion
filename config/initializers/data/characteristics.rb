05ff41e7da005f8233452486d381c4286bd5fa26
Raúl Jiménez <jimenezjrs@gmail.com>
2014-08-05 13:19:43 -0500

    added data initializers for the fondeso app

---
+++ config/initializers/data/characteristics.rb
@@ -0,0 +1,19 @@
+CHARACTERISTICS = [
+  "Activo fijo",
+  "Asesoría/Capacitación",
+  "Beca",
+  "Bolsa de trabajo/Empleabilidad",
+  "Capital",
+  "Capital de trabajo y otros",
+  "Certificación",
+  "Comercialización",
+  "Consultoría",
+  "Garantía para crédito",
+  "Gasto corriente",
+  "Innovación",
+  "Investigación y Desarrollo",
+  "Instrumentos financieros",
+  "Trámites",
+  "Vinculación",
+  "Otros"
+]
---
+++ config/initializers/data/deliver_methods.rb
@@ -0,0 +1,11 @@
+DELIVER_METHODS = [
+  "Bolsa de trabajo/Empleabilidad",
+  "Crédito",
+  "Capital",
+  "Capacitación",
+  "Certificación",
+  "Contratación de servicio",
+  "Subsidio",
+  "Vinculación",
+  "Otro"
+]
