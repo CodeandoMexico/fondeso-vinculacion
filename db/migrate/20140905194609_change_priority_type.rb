acdb8e3785e22485c38ff185b4f6f6db8d83faf3
Raúl Jiménez <jimenezjrs@gmail.com>
2014-09-10 12:33:07 -0500

    edit priority migration

--- db/migrate/20140905194609_change_priority_type.rb
+++
@@ -1,9 +0,0 @@
-class ChangePriorityType < ActiveRecord::Migration
-  def up
-    change_column :funds, :priority, :string
-  end
-
-  def down
-    change_column :funds, :priority, :integer
-  end
-end
