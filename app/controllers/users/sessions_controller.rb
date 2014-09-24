38d3376939d17ea4b38da90504f2fb88d97e7066
Raúl Jiménez <jimenezjrs@gmail.com>
2014-09-24 17:15:32 -0500

    possible to login users

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,12 +3,13 @@ class Users::SessionsController < Devise::SessionsController
     render json: current_user
   end
 
-  protected
-    def after_sign_in_path_for(resource)
-      root_path
-    end
-
-    def after_sign_out_path_for(resource)
-      root_path
-    end
+  # protected
+  #   def after_sign_in_path_for(resource)
+  #     render json: resource
+  #     return
+  #   end
+  #
+  #   def after_sign_out_path_for(resource)
+  #     # current_path
+  #   end
 end
