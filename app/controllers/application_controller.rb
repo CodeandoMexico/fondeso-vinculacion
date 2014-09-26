ed2c8dbe419a77b12d6ae7f217387ffafc58cc0f
Raúl Jiménez <jimenezjrs@gmail.com>
2014-09-25 20:20:12 -0500

    added some changes to the allow origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,11 +24,11 @@ class ApplicationController < ActionController::Base
     # For all responses in this controller, return the CORS access control headers.
 
     def cors_set_access_control_headers
-      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Origin'] = 'http://fondeso-frontend-staging.herokuapp.com'
       headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
       headers['Access-Control-Request-Method'] = '*'
       headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-      headers['Access-Control-Allow-Credentials'] = true
+      headers['Access-Control-Allow-Credentials'] = 'true'
       headers['Access-Control-Max-Age'] = "1728000"
     end
 
@@ -38,11 +38,11 @@ class ApplicationController < ActionController::Base
 
     def cors_preflight_check
       if request.method == :options
-        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Origin'] = 'http://fondeso-frontend-staging.herokuapp.com'
         headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
         headers['Access-Control-Request-Method'] = '*'
         headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-        headers['Access-Control-Allow-Credentials'] = true
+        headers['Access-Control-Allow-Credentials'] = 'true'
         headers['Access-Control-Max-Age'] = '1728000'
         render :text => '', :content_type => 'text/plain'
       end
