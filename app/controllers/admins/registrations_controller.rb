1e30665cbf3274cbf89785385d4bf3b4383b4ed1
Raúl Jiménez <jimenezjrs@gmail.com>
2014-09-28 23:34:49 -0500

    added some security to the admin registration page

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -1,4 +1,14 @@
 class Admins::RegistrationsController < Devise::RegistrationsController
+  def new
+    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+    # redirect_to root_path
+  end
+
+  def create
+    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+    # redirect_to root_path
+  end
+
   protected
     def after_sign_up_path_for(resource)
       funds_path
