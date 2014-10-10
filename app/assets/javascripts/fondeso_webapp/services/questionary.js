fc850d72c0f3e29793a2c32a12173e4f13a40580
Raúl Jiménez <jimenezjrs@gmail.com>
2014-10-10 09:58:19 -0500

    changed the url back to fondeso.herokuapp

--- app/assets/javascripts/fondeso_webapp/services/questionary.js
+++ app/assets/javascripts/fondeso_webapp/services/questionary.js
@@ -741,8 +741,8 @@ angular.module('questionaryApp')
     // console.log(questionary);
     questionary.sections['1.B'].questions[3].body.options[1].question = angular.copy(delegationQuestion);
     questionary.sections['1.B'].questions[4].body.options[1].question = angular.copy(delegationQuestion);
-    var baseUrl = 'http://localhost:3000/profile/';
-    // var baseUrl = 'http://fondeso.herokuapp.com/profile/';
+    // var baseUrl = 'http://localhost:3000/profile/';
+    var baseUrl = 'http://fondeso.herokuapp.com/profile/';
     questionary.save = null;
 
     questionary.submit = function(answers, filters, priorities, delegations, profiles, tieAnswers) {
