391644d5d5950178d16e32e1070f215a95a605c8
Raúl Jiménez <jimenezjrs@gmail.com>
2014-10-21 19:51:20 -0500

    users are now exportable

--- app/controllers/dashboard/users_controller.rb
+++ app/controllers/dashboard/users_controller.rb
@@ -2,7 +2,11 @@ class Dashboard::UsersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @users = User.all.order('updated_at DESC').page(params[:page])
-    # raise @users.inspect
+    respond_to do |format|
+      format.html {
+        @users = User.all.order('updated_at DESC').page(params[:page])
+      }
+      format.csv { send_data User.to_csv }
+    end
   end
 end
